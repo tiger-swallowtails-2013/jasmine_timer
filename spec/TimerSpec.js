c20f9050564f24c49be8e1883085c06308725208
Strand McCutchen <strand@bettermistak.es>
2013-09-25 10:08:36 -0700

    Create TimerSpec

---
+++ spec/TimerSpec.js
@@ -0,0 +1,9 @@
+describe("Timer", function() {
+  it("should record the start time when startTime is called", function() {
+
+  });
+  xit("start call Date.now", function() { });
+  xit("should record the end time when endTime is called", function() { });
+  xit("end call Date.now", function() { });
+  xit("should be able to calculate time", function() { });
+});
